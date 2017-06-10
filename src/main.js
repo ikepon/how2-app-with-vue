486ba19c36b3234f7c07a790c438ac6b421baf22
ikepon <tatsuyanoheya@gmail.com>
2017-06-10 14:48:05 +0900

    Vue router で検索を定義

--- src/main.js
+++ src/main.js
@@ -11,6 +11,5 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App },
+  render: h => h(App),
 });
--- src/router.js
+++ src/router.js
@@ -1,15 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
 
+import Search from './StackOverflow/Search';
+
+// Install the plugin
 Vue.use(Router);
 
+// Create a router
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
+      component: Search,
     },
   ],
 });
